ce695e59a8e583d0f436e3a93ef916bcfaea6be5
tianzhiya <569500735@qq.com>
2025-06-24 07:59:16 +0800

    init

--- dataset/RegDataset.py
+++ dataset/RegDataset.py
@@ -32,12 +32,10 @@ class CityScapes(Dataset):
             labels_info = json.load(fr)
         self.lb_map = {el['id']: el['trainId'] for el in labels_info}
 
-        # impth = osp.join(rootpth, Method, mode)
         self.img_dir = os.path.join(rootpth, 'fused')
         self.label_dir = os.path.join(rootpth, 'label')
         self.file_list = natsorted(os.listdir(self.img_dir))
 
-        ## pre-processing
         self.to_tensor = transforms.Compose(
             [
                 transforms.ToTensor(),
@@ -59,29 +57,25 @@ class CityScapes(Dataset):
 
 
 class RegData(torch.utils.data.Dataset):
-    """
-    Load dataset with infrared folder path and visible folder path
-    """
 
     def __init__(self, opts, crop=lambda x: x):
         super(RegData, self).__init__()
         self.vis_folder = os.path.join(opts.dataroot, 'vi')
         self.ir_folder = os.path.join(opts.dataroot, 'ir')
         self.crop = torchvision.transforms.RandomCrop(256)
-        # gain infrared and visible images list
+
         self.vis_list = sorted(os.listdir(self.vis_folder))
         self.ir_list = sorted(os.listdir(self.ir_folder))
         print(len(self.vis_list), len(self.ir_list))
 
     def __getitem__(self, index):
-        # gain image path
         vis_path = os.path.join(self.vis_folder, self.vis_list[index])
         ir_path = os.path.join(self.ir_folder, self.ir_list[index])
 
         assert os.path.basename(vis_path) == os.path.basename(
             ir_path), f"Mismatch ir:{os.path.basename(ir_path)} vi:{os.path.basename(vis_path)}."
 
-        # read image as type Tensor
+
         vis = self.imread(path=vis_path, flags=cv2.IMREAD_GRAYSCALE)
         ir = self.imread(path=ir_path, flags=cv2.IMREAD_GRAYSCALE)
 
@@ -99,11 +93,8 @@ class RegData(torch.utils.data.Dataset):
         vis, ir, vis_warped, ir_warped, disp = torch.split(patch, [3, 3, 3, 3, 2], dim=1)
         h, w = vis_ir.shape[2], vis_ir.shape[3]
         scale = (torch.FloatTensor([w, h]).unsqueeze(0).unsqueeze(0) - 1) / (self.crop.size[0] * 1.0 - 1)
-        # print(self.crop.size[0])
         disp = disp.permute(0, 2, 3, 1) * scale
-        # vis_warped_ = self.ST(vis.unsqueeze(0),disp.unsqueeze(0))
-        # TF.to_pil_image(((vis_warped-vis_warped_).abs()).squeeze(0)).save('error.png')
-        # disp_crop = disp[:,h//2-150:h//2+150,w//2-150:w//2+150,:]*scale
+
         return ir, vis, ir_warped, vis_warped, disp
 
     def __len__(self):
@@ -111,18 +102,14 @@ class RegData(torch.utils.data.Dataset):
 
     @staticmethod
     def imread(path, flags=cv2.IMREAD_GRAYSCALE):
-        # im_cv = cv2.imread(str(path), flags)
-        # assert im_cv is not None, f"Image {str(path)} is invalid."
-        # im_ts = kornia.Tools.image_to_tensor(im_cv / 255.,keepdim=False).type(torch.FloatTensor)
+
         img = Image.open(path).convert('RGB')
         im_ts = TF.to_tensor(img).unsqueeze(0)
         return im_ts
 
 
 class MSRSData(torch.utils.data.Dataset):
-    """
-    Load dataset with infrared folder path and visible folder path
-    """
+
 
     def __init__(self, opts, crop=lambda x: x):
         super(MSRSData, self).__init__()
@@ -137,12 +124,10 @@ class MSRSData(torch.utils.data.Dataset):
         print(len(self.vis_list), len(self.ir_list), len(self.label_list))
 
     def __getitem__(self, index):
-        # gain image path
         image_name = self.ir_list[index]
         vis_path = os.path.join(self.vis_folder, image_name)
         ir_path = os.path.join(self.ir_folder, image_name)
         label_path = os.path.join(self.label_folder, image_name)
-        # read image as type Tensor
         vis = self.imread(path=vis_path)
         ir = self.imread(path=ir_path)
         label = self.imread(path=label_path, label=True)
@@ -174,9 +159,7 @@ class MSRSData(torch.utils.data.Dataset):
 
 
 class RoadSceneData(torch.utils.data.Dataset):
-    """
-    Load dataset with infrared folder path and visible folder path
-    """
+
 
 
     def __init__(self, opts, crop=lambda x: x):
@@ -184,18 +167,14 @@ class RoadSceneData(torch.utils.data.Dataset):
         self.vis_folder = os.path.join(opts.dataroot, 'vi')
         self.ir_folder = os.path.join(opts.dataroot, 'ir')
         self.crop = torchvision.transforms.RandomCrop(256)
-        # gain infrared and visible images list
         self.vis_list = sorted(os.listdir(self.vis_folder))
         self.ir_list = sorted(os.listdir(self.ir_folder))
         print(len(self.vis_list), len(self.ir_list))
-        # self.ST = SpatialTransformer(self.crop.size[0],self.crop.size[0],False)
 
     def __getitem__(self, index):
-        # gain image path
         image_name = self.ir_list[index]
         vis_path = os.path.join(self.vis_folder, image_name)
         ir_path = os.path.join(self.ir_folder, image_name)
-        # read image as type Tensor
         vis = self.imread(path=vis_path)
         ir = self.imread(path=ir_path)
 
@@ -224,23 +203,18 @@ class RoadSceneData(torch.utils.data.Dataset):
 
 
 class TestData(torch.utils.data.Dataset):
-    """
-    Load dataset with infrared folder path and visible folder path
-    """
+
 
     def __init__(self, ir_dir=None, vi_dir=None):
         super(TestData, self).__init__()
         self.vis_folder = vi_dir
         self.ir_folder = ir_dir
-        # gain infrared and visible images list
         self.ir_list = natsorted(os.listdir(self.ir_folder))
 
     def __getitem__(self, index):
-        # gain image path
         image_name = self.ir_list[index]
         vis_path = os.path.join(self.vis_folder, image_name)
         ir_path = os.path.join(self.ir_folder, image_name)
-        # read image as type Tensor
         vis = self.imread(path=vis_path)
         ir = self.imread(path=ir_path)
         return ir, vis, image_name
